67ea251d32076c8941d4536d83b54a488edb05d1
rohit kumar <rohit.kumar@comfyapp.com>
2024-09-17 17:47:05 +0530

    add

---
+++ classes/construc_to_func.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std
+
+
+
+class construc_to_func
+{
+private:
+    /* data */
+public:
+    construc_to_func(/* args */);
+    ~construc_to_func();
+    void display(){
+        cout<<"it is display";
+    }
+};
+
+construc_to_func::construc_to_func(/* args */)
+{
+
+};
+
+construc_to_func::~construc_to_func()
+{
+};
