58f7697514ec232daf24ad4efe32af3b26f2ee82
rohit kumar <12rohit4@gmail.com>
2025-09-29 09:23:32 +0000

    "incomplete linked list , but jsut wanted to commit"

--- linked_list/ll.cpp
+++ linked_list/ll.cpp
@@ -1,8 +1,32 @@
 class Node{
-int element;
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr
+    }
 };
 
 
 class linked_list{
+private:
+    Node* head;
+public:
+    linked_list (int val) {
+        head = new Node(val):
+
+    }
+
+    void append(int val) {
+        Node* newNode = new Node(val);
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
 
 };
